bc8a4dfc907e51f3665c09e12ab459e98b61b562
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-08-16 21:28:47 +0530

    Create 26_removeDuplicates.py

---
+++ LeetCode/Easy/26_removeDuplicates.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums: return 0        
+        nextUnique=1
+        for i in range(1,len(nums)):
+            if (nums[i]!=nums[nextUnique-1]):
+               nums[nextUnique]=nums[i]
+               nextUnique+=1
+        return nextUnique
