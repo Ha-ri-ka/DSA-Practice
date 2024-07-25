92681a37ce1f12049139aa9ee60d87fadb5cc05c
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2024-07-25 19:40:48 +0530

    Update 0118_PascalsTriangle.py

--- LeetCode/Easy/0118_PascalsTriangle.py
+++ LeetCode/Easy/0118_PascalsTriangle.py
@@ -2,9 +2,7 @@ class Solution(object):
     def generate(self, numRows):
         triangle=[[1]]
         if numRows==1: return triangle
-        triangle=[[1],[1,1]]
-        if numRows==2: return triangle
-        for i in range(2,numRows):
+        for i in range(1,numRows):
             row=[1]
             prev=triangle[i-1]
             for j in range(len(prev)-1):
@@ -12,7 +10,4 @@ class Solution(object):
                 row.append(ele)
             row.append(1)
             triangle.append(row)
-        return triangle
-
-
-        
+        return triangle       
