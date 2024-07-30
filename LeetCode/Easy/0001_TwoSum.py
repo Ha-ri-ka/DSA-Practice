2eff10e7ea2889c54de3a4406bca515a00cf768c
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2024-07-30 22:21:16 +0530

    Update 0001_TwoSum.py

--- LeetCode/Easy/0001_TwoSum.py
+++ LeetCode/Easy/0001_TwoSum.py
@@ -1,13 +1,23 @@
-class Solution(object):
-    #solution 0:    
-    def twoSum(self, nums, target):
-        indices=[]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums)==2: 
+            return [0,1]
         n=len(nums)
+        #brute force----------------
+        # for i in range(n):
+        #     ele=nums[i]
+        #     for j in range(i+1,len(nums)):
+        #         ans=0
+        #         ans=ele+nums[j]
+        #         if ans==target:
+        #             return [i,j]
+
+        #better---------------------
+        indicesMap={}
         for i in range(n):
-            Twosum=0
-            Twosum+=nums[i]
-            for j in range (i+1,n):
-                if (Twosum+nums[j]==target):
-                    indices.append(i)
-                    indices.append(j)
-        return indices      
+            indicesMap[nums[i]]=i
+        print(indicesMap)   
+        for i in range(n):
+            ele2=target-nums[i]
+            if ele2 in indicesMap and indicesMap[ele2]!=i:
+                return [i,indicesMap[ele2]] 
